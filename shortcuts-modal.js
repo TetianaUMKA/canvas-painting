8ee5f7719e70549ddea6c7c3cf5666dc56e82dbc
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2024-01-02 21:29:14 +0200

    Update shortcuts-modal.js

--- shortcuts-modal.js
+++ shortcuts-modal.js
@@ -1,1 +1,22 @@
-const activeShortcutsModal = "modal-active";
+"use strict";
+
+const shortcutsModal = document.querySelector(".shortcuts-modal-container");
+const overlay = document.querySelector(".overlay");
+const openShortcutsModalBtn = document.querySelector(".shortcuts-open-btn");
+const closeShortcutsModalBtn = document.querySelector(
+  "#close-shortcuts-modal-btn"
+);
+
+const openShortcutsModal = function () {
+  shortcutsModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  console.log("click");
+};
+const closeShortcutsModal = function () {
+  shortcutsModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+openShortcutsModalBtn.addEventListener("click", openShortcutsModal);
+closeShortcutsModalBtn.addEventListener("click", closeShortcutsModal);
+overlay.addEventListener("click", closeShortcutsModal);
